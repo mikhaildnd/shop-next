f308c1f061d6e776aae96aac628a4d4cd43c767e
MikhailDnd <dundukovmi@gmail.com>
2026-05-06 20:33:36 +0500

    add Logo

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import LogoImage from '../../../public/icons/logo.png';
+import clsx from 'clsx';
+
+const Logo = ({ className }: { className?: string }) => {
+    return (
+        <Link
+            href="/"
+            aria-label="На главную"
+            className={clsx('flex items-center', className)}
+        >
+            <Image
+                className="shrink-0"
+                src={LogoImage}
+                alt="Логотип магазина"
+                priority
+                width={40}
+                height={40}
+            />
+        </Link>
+    );
+};
+
+export default Logo;
