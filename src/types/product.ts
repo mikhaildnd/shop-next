741bfc56d149081f5cc07190477b93161eda0f81
MikhailDnd <dundukovmi@gmail.com>
2026-05-12 17:05:00 +0500

    upd Product type

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,12 +1,33 @@
-export interface ProductCardProps {
-    id: number;
+import { ObjectId } from 'mongodb';
+
+type ProductMeasure =
+    | {
+          weight: string;
+          volume?: never;
+      }
+    | {
+          volume: string;
+          weight?: never;
+      };
+
+interface ProductBase<TId> {
+    _id: TId;
+
+    slug: string;
+
     img: string;
     title: string;
     description: string;
+
     basePrice: number;
     discountPercent?: number;
     rating: number;
-    weight?: string;
+
     categories: string[];
-    // volume?: string;
 }
+
+export type ProductDto = ProductBase<string> & ProductMeasure;
+
+export type ProductDocument = ProductBase<ObjectId> & ProductMeasure;
+
+export type ProductSeed = Omit<ProductDocument, '_id'>;
