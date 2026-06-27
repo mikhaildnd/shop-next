5b8f43e3fefbb4a4cc07dd0839ae768aaf593d8d
MikhailDnd <dundukovmi@gmail.com>
2026-06-27 21:00:32 +0500

    style: adjust home page spacing

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -10,7 +10,7 @@ import { slides } from '@/data/mainSlides';
 
 export default async function HomePage() {
     return (
-        <div className="page-spacing flex flex-col gap-y-10 md:gap-y-20">
+        <div className="page-spacing flex flex-col gap-y-10 md:mt-6 md:gap-y-20 lg:mt-10">
             <div className="-mx-(--section-padding) md:mx-0">
                 <MainSlider
                     className="h-[500px] w-full rounded-b-3xl md:rounded-3xl xl:h-[680px]"
