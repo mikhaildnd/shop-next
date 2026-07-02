340b7464751a121e1ab730479168005a80802d9c
MikhailDnd <dundukovmi@gmail.com>
2026-07-02 05:30:12 +0500

    refactor(services): move dto types to service domains

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -1,6 +1,7 @@
 import { prisma } from '@/lib/db';
 import { mapUserToDto } from '@/lib/mappers/user.mapper';
-import type { UserDto } from '@/types/user';
+
+import type { UserDto } from '@/services/user/user.types';
 
 export async function getUsers(): Promise<UserDto[]> {
     const users = await prisma.user.findMany({});
