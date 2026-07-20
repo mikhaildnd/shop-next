a502bccbdcc383686fdb1e28a7e50bc626712ef5
MikhailDnd <dundukovmi@gmail.com>
2026-07-20 13:54:22 +0500

    chore(next): allow additional image qualities

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     images: {
-        qualities: [70, 75, 80],
+        qualities: [70, 75, 80, 90, 100],
     },
     turbopack: {
         rules: {
