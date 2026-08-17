f4ed2bd5420a28f97b6ece503c0f04c294b2ad04
MikhailDnd <dundukovmi@gmail.com>
2026-08-17 18:50:34 +0500

    feat(favorites): add favorites data model

---
+++ prisma/migrations/20260817134624_add_favorites/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "favorites" (
+    "user_id" TEXT NOT NULL,
+    "product_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "favorites_pkey" PRIMARY KEY ("user_id","product_id")
+);
+
+-- CreateIndex
+CREATE INDEX "favorites_product_id_idx" ON "favorites"("product_id");
+
+-- AddForeignKey
+ALTER TABLE "favorites" ADD CONSTRAINT "favorites_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "favorites" ADD CONSTRAINT "favorites_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE CASCADE ON UPDATE CASCADE;
