b38c212a9e724f823226756defc7a630f677c5f2
MikhailDnd <dundukovmi@gmail.com>
2026-09-02 15:54:20 +0500

    chore(ui): initialize shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,8 @@ import type { Metadata } from 'next';
 import { Rubik } from 'next/font/google';
 import type { ReactNode } from 'react';
 
-import { cn } from '@/lib/cn';
-
 const fontMain = Rubik({
-    variable: '--font-main',
+    variable: '--font-sans',
     subsets: ['latin', 'cyrillic'],
     weight: ['300', '400', '500', '600', '700', '800'],
 });
@@ -23,13 +21,11 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
     return (
-        <html lang="ru">
-            <body
-                className={cn(
-                    'flex min-h-screen flex-col antialiased',
-                    fontMain.variable,
-                )}
-            >
+        <html
+            lang="ru"
+            className={fontMain.variable}
+        >
+            <body className="flex min-h-screen flex-col antialiased">
                 {children}
             </body>
         </html>
