952411e2c5409dc65585830361e55942a21604eb
MikhailDnd <dundukovmi@gmail.com>
2026-09-03 15:08:59 +0500

    change(auth): redirect to home page after sign-in

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -10,7 +10,7 @@ export default async function SignInPage() {
     const session = await getSession();
 
     if (session) {
-        redirect(routes.profilePage());
+        redirect(routes.homePage());
     }
 
     return (
